36e0a62474a21eee43afcd18d31aba2d06dba565
Cooper Luetje <cooperluetje@fryar-win10-tab.ycp.edu>
2016-09-25 12:08:23 -0400

    Updated Basic UI

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -11,17 +11,17 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate
 {
     // MARK: Properties
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
+    //@IBOutlet weak var nameTextField: UITextField!
+    //@IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         // Handle the text field's user input through delegate callbacks
-        nameTextField.delegate = self
+        //nameTextField.delegate = self
     }
-    
+    /*
     // MARK: UITextFieldDelegate
     func textFieldShouldReturn(textField: UITextField) -> Bool
     {
@@ -39,6 +39,6 @@ class ViewController: UIViewController, UITextFieldDelegate
     {
         mealNameLabel.text = "Default Text"
     }
-    
+    */
 }
 
