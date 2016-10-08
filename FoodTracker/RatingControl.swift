b3de9c29e92da7d39062b59434ccfe850b3d88c5
Cooper Luetje <cooperluetje@coopers-mac-mini.local>
2016-10-08 16:03:13 -0400

    Finished buttons before stars images

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -13,13 +13,15 @@ class RatingControl: UIView
     // MARK: Properties
     var rating = 0
     var ratingButtons = [UIButton]()
+    let spacing = 5
+    let starCount = 5
 
     // MARK: Initialization
     required init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
         
-        for _ in 0..<5
+        for _ in 0..<starCount
         {
             let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
             button.backgroundColor = UIColor.red
@@ -31,17 +33,22 @@ class RatingControl: UIView
     
     override var intrinsicContentSize : CGSize
     {
-        return CGSize(width: 240, height: 44)
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
     }
     
     override func layoutSubviews()
     {
-        var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
+        // Set the button's width and height to a square the size of the frame's height
+        let buttonSize = Int(frame.size.height)
+        var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
         
         // Offset each button's origin by the length of the button plus spacing.
         for(index, button) in ratingButtons.enumerated()
         {
-            buttonFrame.origin.x = CGFloat(index * (44+5))
+            buttonFrame.origin.x = CGFloat(index * (buttonSize+spacing))
             button.frame = buttonFrame
         }
     }
