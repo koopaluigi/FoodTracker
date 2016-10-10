da2da9e64deb60ea28e09c9091a850ae70a3b94f
Cooper Luetje <cooperluetje@coopers-mac-mini.local>
2016-10-10 00:54:40 -0400

    Create a Table View

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Cooper Luetje on 10/9/16.
+//
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell
+{
+    // MARK: Properties
+    @IBOutlet weak var mealLabel: UILabel! //Mistake
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
